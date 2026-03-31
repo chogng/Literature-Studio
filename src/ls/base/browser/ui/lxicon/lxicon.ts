6230ff9129ba21165f93b05c11690edf20926432
Lance <lance@LancedeMac-Studio.local>
2026-03-31 14:20:12 -0700

    feat(lxicon): refresh icon set

--- src/ls/base/browser/ui/lxicon/lxicon.ts
+++ src/ls/base/browser/ui/lxicon/lxicon.ts
@@ -1,92 +1,458 @@
 import './lxicon.css';
-import addSvg from './svg/add.svg?raw';
-import addFileSvg from './svg/add-file.svg?raw';
-import arrowDownSvg from './svg/arrow-down.svg?raw';
-import arrowLeftSvg from './svg/arrow-left.svg?raw';
-import arrowRightSvg from './svg/arrow-right.svg?raw';
-import arrowUpSvg from './svg/arrow-up.svg?raw';
-import checkSvg from './svg/check.svg?raw';
-import chevronDownSvg from './svg/chevron-down.svg?raw';
-import chevronRightSvg from './svg/chevron-right.svg?raw';
-import closeSvg from './svg/close.svg?raw';
-import downloadSvg from './svg/download.svg?raw';
-import enterSvg from './svg/enter.svg?raw';
-import exportSvg from './svg/export.svg?raw';
-import filePdfSvg from './svg/file-pdf.svg?raw';
-import gearSvg from './svg/gear.svg?raw';
-import historySvg from './svg/history.svg?raw';
-import imageSvg from './svg/image.svg?raw';
-import layoutSidebarLeftSvg from './svg/layout-sidebar-left.svg?raw';
-import layoutSidebarLeftOffSvg from './svg/layout-sidebar-left-off.svg?raw';
+import zoomOutSvg from './svg/zoom-out.svg?raw';
+import zoomInSvg from './svg/zoom-in.svg?raw';
+import warningSvg from './svg/warning.svg?raw';
+import voiceCircleSvg from './svg/voice-circle.svg?raw';
+import voiceCircleFilledSvg from './svg/voice-circle-filled.svg?raw';
+import unlimitedSvg from './svg/unlimited.svg?raw';
+import unfoldSvg from './svg/unfold.svg?raw';
+import typescriptSvg from './svg/typescript.svg?raw';
+import tsSvg from './svg/ts.svg?raw';
+import trashSvg from './svg/trash.svg?raw';
+import translateSvg from './svg/translate.svg?raw';
+import threeBarsSvg from './svg/three-bars.svg?raw';
+import textSvg from './svg/text.svg?raw';
+import textSizeSvg from './svg/text-size.svg?raw';
+import terminalSvg from './svg/terminal.svg?raw';
+import terminalGitBashSvg from './svg/terminal-git-bash.svg?raw';
+import terminalCmdSvg from './svg/terminal-cmd.svg?raw';
+import tasklistSvg from './svg/tasklist.svg?raw';
+import syncSvg from './svg/sync.svg?raw';
+import symbolNumericSvg from './svg/symbol-numeric.svg?raw';
+import stopCircleSvg from './svg/stop-circle.svg?raw';
+import startSvg from './svg/start.svg?raw';
+import splitVerticalSvg from './svg/split-vertical.svg?raw';
+import splitHorizontalSvg from './svg/split-horizontal.svg?raw';
+import settingsSvg from './svg/settings.svg?raw';
+import selectSvg from './svg/select.svg?raw';
+import searchSvg from './svg/search.svg?raw';
+import screenFullSvg from './svg/screen-full.svg?raw';
+import saveSvg from './svg/save.svg?raw';
+import sandboxSvg from './svg/sandbox.svg?raw';
+import rssSvg from './svg/rss.svg?raw';
+import repoPushSvg from './svg/repo-push.svg?raw';
+import repoPullSvg from './svg/repo-pull.svg?raw';
+import repoFetchSvg from './svg/repo-fetch.svg?raw';
+import removeSvg from './svg/remove.svg?raw';
+import refreshSvg from './svg/refresh.svg?raw';
+import quotesSvg from './svg/quotes.svg?raw';
+import quoteSvg from './svg/quote.svg?raw';
+import questionSvg from './svg/question.svg?raw';
+import previewSvg from './svg/preview.svg?raw';
+import previewOffSvg from './svg/preview-off.svg?raw';
+import pinnedSvg from './svg/pinned.svg?raw';
+import pdfSvg from './svg/pdf.svg?raw';
+import pauseSvg from './svg/pause.svg?raw';
+import originSvg from './svg/origin.svg?raw';
+import openaiSvg from './svg/openai.svg?raw';
+import noteSvg from './svg/note.svg?raw';
+import moreSvg from './svg/more.svg?raw';
+import modelSvg from './svg/model.svg?raw';
+import micSvg from './svg/mic.svg?raw';
+import micFilledSvg from './svg/mic-filled.svg?raw';
+import messageMoreSvg from './svg/message-more.svg?raw';
+import mentionSvg from './svg/mention.svg?raw';
+import mdSvg from './svg/md.svg?raw';
+import markdownSvg from './svg/markdown.svg?raw';
+import lockSvg from './svg/lock.svg?raw';
+import localSvg from './svg/local.svg?raw';
+import listUnorderedSvg from './svg/list-unordered.svg?raw';
+import listUnfoldSvg from './svg/list-unfold.svg?raw';
+import listOrderedSvg from './svg/list-ordered.svg?raw';
+import listFoldSvg from './svg/list-fold.svg?raw';
+import linkExternalSvg from './svg/link-external.svg?raw';
+import librarySvg from './svg/library.svg?raw';
 import layoutSidebarRightSvg from './svg/layout-sidebar-right.svg?raw';
 import layoutSidebarRightOffSvg from './svg/layout-sidebar-right-off.svg?raw';
-import librarySvg from './svg/library.svg?raw';
-import linkExternalSvg from './svg/link-external.svg?raw';
-import moreSvg from './svg/more.svg?raw';
-import micSvg from './svg/mic.svg?raw';
-import settingsSvg from './svg/settings.svg?raw';
-import syncSvg from './svg/sync.svg?raw';
-import refreshSvg from './svg/refresh.svg?raw';
+import layoutSidebarLeftSvg from './svg/layout-sidebar-left.svg?raw';
+import layoutSidebarLeftOffSvg from './svg/layout-sidebar-left-off.svg?raw';
+import layoutPanelSvg from './svg/layout-panel.svg?raw';
+import layoutPanelOffSvg from './svg/layout-panel-off.svg?raw';
+import latticeDarkSvg from './svg/lattice-dark.svg?raw';
+import keyboardTabSvg from './svg/keyboard-tab.svg?raw';
+import jsSvg from './svg/js.svg?raw';
+import javascriptSvg from './svg/javascript.svg?raw';
+import italicsSvg from './svg/italics.svg?raw';
+import infoSvg from './svg/info.svg?raw';
+import imageSvg from './svg/image.svg?raw';
+import imageFilledSvg from './svg/image-filled.svg?raw';
+import htmlSvg from './svg/html.svg?raw';
+import historySvg from './svg/history.svg?raw';
+import githubSvg from './svg/Github.svg?raw';
+import githubInvertedSvg from './svg/Github-inverted.svg?raw';
+import githubAltSvg from './svg/Github-alt.svg?raw';
+import gitSvg from './svg/git.svg?raw';
+import gitPullRequestSvg from './svg/git-pull-request.svg?raw';
+import gitCommitSvg from './svg/git-commit.svg?raw';
+import gitBranchSvg from './svg/git-branch.svg?raw';
+import gearSvg from './svg/gear.svg?raw';
+import followSvg from './svg/follow.svg?raw';
+import foldersSvg from './svg/folders.svg?raw';
+import folderSvg from './svg/folder.svg?raw';
+import folderOpenSvg from './svg/folder-open.svg?raw';
+import foldSvg from './svg/fold.svg?raw';
+import filterSvg from './svg/filter.svg?raw';
+import filesSvg from './svg/files.svg?raw';
+import fileSvg from './svg/file.svg?raw';
+import filePdfSvg from './svg/file-pdf.svg?raw';
+import fileDocxSvg from './svg/file-docx.svg?raw';
+import extensionsSvg from './svg/extensions.svg?raw';
+import exportSvg from './svg/export.svg?raw';
+import export2Svg from './svg/export-2.svg?raw';
+import errorSvg from './svg/error.svg?raw';
+import enterSvg from './svg/enter.svg?raw';
+import downloadSvg from './svg/download.svg?raw';
+import download2Svg from './svg/download-2.svg?raw';
+import docxSvg from './svg/docx.svg?raw';
+import discardSvg from './svg/discard.svg?raw';
+import diffRemovedSvg from './svg/diff-removed.svg?raw';
+import diffAddedSvg from './svg/diff-added.svg?raw';
+import databaseSvg from './svg/database.svg?raw';
+import cursorSvg from './svg/cursor.svg?raw';
+import cornerTopRightSvg from './svg/corner-top-right.svg?raw';
+import cornerTopLeftSvg from './svg/corner-top-left.svg?raw';
+import cornerBottomRightSvg from './svg/corner-bottom-right.svg?raw';
+import cornerBottomLeftSvg from './svg/corner-bottom-left.svg?raw';
+import copySvg from './svg/copy.svg?raw';
+import commentSvg from './svg/comment.svg?raw';
+import commandSvg from './svg/command.svg?raw';
+import commandSuccessSvg from './svg/command-success.svg?raw';
+import commandStartSvg from './svg/command-start.svg?raw';
+import commandErrorSvg from './svg/command-error.svg?raw';
+import codeSvg from './svg/code.svg?raw';
+import codeReviewSvg from './svg/code-review.svg?raw';
+import cloudSvg from './svg/cloud.svg?raw';
+import closeSvg from './svg/close.svg?raw';
+import circleSlashSvg from './svg/circle-slash.svg?raw';
+import chevronUpSvg from './svg/chevron-up.svg?raw';
+import chevronRightSvg from './svg/chevron-right.svg?raw';
+import chevronLeftSvg from './svg/chevron-left.svg?raw';
+import chevronDownSvg from './svg/chevron-down.svg?raw';
+import checklistSvg from './svg/checklist.svg?raw';
+import checkSvg from './svg/check.svg?raw';
+import broswerSvg from './svg/broswer.svg?raw';
+import brainSvg from './svg/brain.svg?raw';
+import bracketSvg from './svg/bracket.svg?raw';
+import bookSvg from './svg/book.svg?raw';
+import boldSvg from './svg/bold.svg?raw';
+import bellSvg from './svg/bell.svg?raw';
+import bellSlashSvg from './svg/bell-slash.svg?raw';
+import bellDotSvg from './svg/bell-dot.svg?raw';
+import arrowUpSvg from './svg/arrow-up.svg?raw';
+import arrowRightSvg from './svg/arrow-right.svg?raw';
+import arrowLeftSvg from './svg/arrow-left.svg?raw';
+import arrowDownSvg from './svg/arrow-down.svg?raw';
+import arrowCircleUpSvg from './svg/arrow-circle-up.svg?raw';
+import arrowCircleRightSvg from './svg/arrow-circle-right.svg?raw';
+import arrowCircleLeftSvg from './svg/arrow-circle-left.svg?raw';
+import arrowCircleDownSvg from './svg/arrow-circle-down.svg?raw';
+import addSvg from './svg/add.svg?raw';
+import addFileSvg from './svg/add-file.svg?raw';
+import accountSvg from './svg/account.svg?raw';
 
 export type LxIconName =
-  | 'add'
-  | 'add-file'
-  | 'arrow-down'
-  | 'arrow-left'
-  | 'arrow-right'
-  | 'arrow-up'
-  | 'check'
-  | 'chevron-down'
-  | 'chevron-right'
-  | 'close'
-  | 'download'
-  | 'enter'
-  | 'export'
-  | 'file-pdf'
-  | 'gear'
-  | 'history'
-  | 'image'
-  | 'layout-sidebar-left'
-  | 'layout-sidebar-left-off'
+  | 'zoom-out'
+  | 'zoom-in'
+  | 'warning'
+  | 'voice-circle'
+  | 'voice-circle-filled'
+  | 'unlimited'
+  | 'unfold'
+  | 'typescript'
+  | 'ts'
+  | 'trash'
+  | 'translate'
+  | 'three-bars'
+  | 'text'
+  | 'text-size'
+  | 'terminal'
+  | 'terminal-git-bash'
+  | 'terminal-cmd'
+  | 'tasklist'
+  | 'sync'
+  | 'symbol-numeric'
+  | 'stop-circle'
+  | 'start'
+  | 'split-vertical'
+  | 'split-horizontal'
+  | 'settings'
+  | 'select'
+  | 'search'
+  | 'screen-full'
+  | 'save'
+  | 'sandbox'
+  | 'rss'
+  | 'repo-push'
+  | 'repo-pull'
+  | 'repo-fetch'
+  | 'remove'
+  | 'refresh'
+  | 'quotes'
+  | 'quote'
+  | 'question'
+  | 'preview'
+  | 'preview-off'
+  | 'pinned'
+  | 'pdf'
+  | 'pause'
+  | 'origin'
+  | 'openai'
+  | 'note'
+  | 'more'
+  | 'model'
+  | 'mic'
+  | 'mic-filled'
+  | 'message-more'
+  | 'mention'
+  | 'md'
+  | 'markdown'
+  | 'lock'
+  | 'local'
+  | 'list-unordered'
+  | 'list-unfold'
+  | 'list-ordered'
+  | 'list-fold'
+  | 'link-external'
+  | 'library'
   | 'layout-sidebar-right'
   | 'layout-sidebar-right-off'
-  | 'library'
-  | 'link-external'
-  | 'more'
-  | 'mic'
-  | 'settings'
-  | 'sync'
-  | 'refresh';
+  | 'layout-sidebar-left'
+  | 'layout-sidebar-left-off'
+  | 'layout-panel'
+  | 'layout-panel-off'
+  | 'lattice-dark'
+  | 'keyboard-tab'
+  | 'js'
+  | 'javascript'
+  | 'italics'
+  | 'info'
+  | 'image'
+  | 'image-filled'
+  | 'html'
+  | 'history'
+  | 'Github'
+  | 'Github-inverted'
+  | 'Github-alt'
+  | 'git'
+  | 'git-pull-request'
+  | 'git-commit'
+  | 'git-branch'
+  | 'gear'
+  | 'follow'
+  | 'folders'
+  | 'folder'
+  | 'folder-open'
+  | 'fold'
+  | 'filter'
+  | 'files'
+  | 'file'
+  | 'file-pdf'
+  | 'file-docx'
+  | 'extensions'
+  | 'export'
+  | 'export-2'
+  | 'error'
+  | 'enter'
+  | 'download'
+  | 'download-2'
+  | 'docx'
+  | 'discard'
+  | 'diff-removed'
+  | 'diff-added'
+  | 'database'
+  | 'cursor'
+  | 'corner-top-right'
+  | 'corner-top-left'
+  | 'corner-bottom-right'
+  | 'corner-bottom-left'
+  | 'copy'
+  | 'comment'
+  | 'command'
+  | 'command-success'
+  | 'command-start'
+  | 'command-error'
+  | 'code'
+  | 'code-review'
+  | 'cloud'
+  | 'close'
+  | 'circle-slash'
+  | 'chevron-up'
+  | 'chevron-right'
+  | 'chevron-left'
+  | 'chevron-down'
+  | 'checklist'
+  | 'check'
+  | 'broswer'
+  | 'brain'
+  | 'bracket'
+  | 'book'
+  | 'bold'
+  | 'bell'
+  | 'bell-slash'
+  | 'bell-dot'
+  | 'arrow-up'
+  | 'arrow-right'
+  | 'arrow-left'
+  | 'arrow-down'
+  | 'arrow-circle-up'
+  | 'arrow-circle-right'
+  | 'arrow-circle-left'
+  | 'arrow-circle-down'
+  | 'add'
+  | 'add-file'
+  | 'account';
 
 const SVG_BY_NAME: Record<LxIconName, string> = {
-  add: addSvg,
-  'add-file': addFileSvg,
-  'arrow-down': arrowDownSvg,
-  'arrow-left': arrowLeftSvg,
-  'arrow-right': arrowRightSvg,
-  'arrow-up': arrowUpSvg,
-  check: checkSvg,
-  'chevron-down': chevronDownSvg,
-  'chevron-right': chevronRightSvg,
-  close: closeSvg,
-  download: downloadSvg,
-  enter: enterSvg,
-  export: exportSvg,
-  'file-pdf': filePdfSvg,
-  gear: gearSvg,
-  history: historySvg,
-  image: imageSvg,
-  'layout-sidebar-left': layoutSidebarLeftSvg,
-  'layout-sidebar-left-off': layoutSidebarLeftOffSvg,
+  'zoom-out': zoomOutSvg,
+  'zoom-in': zoomInSvg,
+  'warning': warningSvg,
+  'voice-circle': voiceCircleSvg,
+  'voice-circle-filled': voiceCircleFilledSvg,
+  'unlimited': unlimitedSvg,
+  'unfold': unfoldSvg,
+  'typescript': typescriptSvg,
+  'ts': tsSvg,
+  'trash': trashSvg,
+  'translate': translateSvg,
+  'three-bars': threeBarsSvg,
+  'text': textSvg,
+  'text-size': textSizeSvg,
+  'terminal': terminalSvg,
+  'terminal-git-bash': terminalGitBashSvg,
+  'terminal-cmd': terminalCmdSvg,
+  'tasklist': tasklistSvg,
+  'sync': syncSvg,
+  'symbol-numeric': symbolNumericSvg,
+  'stop-circle': stopCircleSvg,
+  'start': startSvg,
+  'split-vertical': splitVerticalSvg,
+  'split-horizontal': splitHorizontalSvg,
+  'settings': settingsSvg,
+  'select': selectSvg,
+  'search': searchSvg,
+  'screen-full': screenFullSvg,
+  'save': saveSvg,
+  'sandbox': sandboxSvg,
+  'rss': rssSvg,
+  'repo-push': repoPushSvg,
+  'repo-pull': repoPullSvg,
+  'repo-fetch': repoFetchSvg,
+  'remove': removeSvg,
+  'refresh': refreshSvg,
+  'quotes': quotesSvg,
+  'quote': quoteSvg,
+  'question': questionSvg,
+  'preview': previewSvg,
+  'preview-off': previewOffSvg,
+  'pinned': pinnedSvg,
+  'pdf': pdfSvg,
+  'pause': pauseSvg,
+  'origin': originSvg,
+  'openai': openaiSvg,
+  'note': noteSvg,
+  'more': moreSvg,
+  'model': modelSvg,
+  'mic': micSvg,
+  'mic-filled': micFilledSvg,
+  'message-more': messageMoreSvg,
+  'mention': mentionSvg,
+  'md': mdSvg,
+  'markdown': markdownSvg,
+  'lock': lockSvg,
+  'local': localSvg,
+  'list-unordered': listUnorderedSvg,
+  'list-unfold': listUnfoldSvg,
+  'list-ordered': listOrderedSvg,
+  'list-fold': listFoldSvg,
+  'link-external': linkExternalSvg,
+  'library': librarySvg,
   'layout-sidebar-right': layoutSidebarRightSvg,
   'layout-sidebar-right-off': layoutSidebarRightOffSvg,
-  library: librarySvg,
-  'link-external': linkExternalSvg,
-  more: moreSvg,
-  mic: micSvg,
-  settings: settingsSvg,
-  sync: syncSvg,
-  refresh: refreshSvg,
+  'layout-sidebar-left': layoutSidebarLeftSvg,
+  'layout-sidebar-left-off': layoutSidebarLeftOffSvg,
+  'layout-panel': layoutPanelSvg,
+  'layout-panel-off': layoutPanelOffSvg,
+  'lattice-dark': latticeDarkSvg,
+  'keyboard-tab': keyboardTabSvg,
+  'js': jsSvg,
+  'javascript': javascriptSvg,
+  'italics': italicsSvg,
+  'info': infoSvg,
+  'image': imageSvg,
+  'image-filled': imageFilledSvg,
+  'html': htmlSvg,
+  'history': historySvg,
+  'Github': githubSvg,
+  'Github-inverted': githubInvertedSvg,
+  'Github-alt': githubAltSvg,
+  'git': gitSvg,
+  'git-pull-request': gitPullRequestSvg,
+  'git-commit': gitCommitSvg,
+  'git-branch': gitBranchSvg,
+  'gear': gearSvg,
+  'follow': followSvg,
+  'folders': foldersSvg,
+  'folder': folderSvg,
+  'folder-open': folderOpenSvg,
+  'fold': foldSvg,
+  'filter': filterSvg,
+  'files': filesSvg,
+  'file': fileSvg,
+  'file-pdf': filePdfSvg,
+  'file-docx': fileDocxSvg,
+  'extensions': extensionsSvg,
+  'export': exportSvg,
+  'export-2': export2Svg,
+  'error': errorSvg,
+  'enter': enterSvg,
+  'download': downloadSvg,
+  'download-2': download2Svg,
+  'docx': docxSvg,
+  'discard': discardSvg,
+  'diff-removed': diffRemovedSvg,
+  'diff-added': diffAddedSvg,
+  'database': databaseSvg,
+  'cursor': cursorSvg,
+  'corner-top-right': cornerTopRightSvg,
+  'corner-top-left': cornerTopLeftSvg,
+  'corner-bottom-right': cornerBottomRightSvg,
+  'corner-bottom-left': cornerBottomLeftSvg,
+  'copy': copySvg,
+  'comment': commentSvg,
+  'command': commandSvg,
+  'command-success': commandSuccessSvg,
+  'command-start': commandStartSvg,
+  'command-error': commandErrorSvg,
+  'code': codeSvg,
+  'code-review': codeReviewSvg,
+  'cloud': cloudSvg,
+  'close': closeSvg,
+  'circle-slash': circleSlashSvg,
+  'chevron-up': chevronUpSvg,
+  'chevron-right': chevronRightSvg,
+  'chevron-left': chevronLeftSvg,
+  'chevron-down': chevronDownSvg,
+  'checklist': checklistSvg,
+  'check': checkSvg,
+  'broswer': broswerSvg,
+  'brain': brainSvg,
+  'bracket': bracketSvg,
+  'book': bookSvg,
+  'bold': boldSvg,
+  'bell': bellSvg,
+  'bell-slash': bellSlashSvg,
+  'bell-dot': bellDotSvg,
+  'arrow-up': arrowUpSvg,
+  'arrow-right': arrowRightSvg,
+  'arrow-left': arrowLeftSvg,
+  'arrow-down': arrowDownSvg,
+  'arrow-circle-up': arrowCircleUpSvg,
+  'arrow-circle-right': arrowCircleRightSvg,
+  'arrow-circle-left': arrowCircleLeftSvg,
+  'arrow-circle-down': arrowCircleDownSvg,
+  'add': addSvg,
+  'add-file': addFileSvg,
+  'account': accountSvg,
 };
 
 function createSvgElement(markup: string) {
